2136695b5c84c74747575d06272cbd3c1295e3c5
badrulameen <73521631+badrulameen@users.noreply.github.com>
2020-12-20 20:20:45 +0700

    Update Text_Editor.py

--- Text_Editor.py
+++ Text_Editor.py
@@ -9,3 +9,11 @@ class Node:
 class doubly_linked_list:
    def __init__(self):
       self.head = None     
+      
+      while input() != '':
+    val += input('')
+
+for element in val:
+    characterList.append(element)
+
+characterList.listprint(characterList.head)
