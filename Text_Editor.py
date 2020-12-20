4b897079bc24daae34a477778edeba0f6ea70750
firdausmhsn <73024064+firdausmhsn@users.noreply.github.com>
2020-12-20 21:22:50 +0800

    Update Text_Editor.py

--- Text_Editor.py
+++ Text_Editor.py
@@ -9,8 +9,24 @@ class Node:
 class doubly_linked_list:
    def __init__(self):
       self.head = None     
+
+# Define the append method to add elements at the end
+   def append(self, NewVal):
+
+      NewNode = Node(NewVal)
+      NewNode.next = None
+      if self.head is None:
+         NewNode.prev = None
+         self.head = NewNode
+         return
+      last = self.head
+      while (last.next is not None): 
+         last = last.next
+      last.next = NewNode
+      NewNode.prev = last
+      return
       
-      while input() != '':
+while input() != '':
     val += input('')
 
 for element in val:
