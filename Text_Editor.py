d6d46c34e9812bd927a65bdb857d0e8a1310500b
muhammad hafiz b mohd jailani <73322298+hafizjailanii@users.noreply.github.com>
2020-12-20 21:17:46 +0800

    Update Text_Editor.py

--- Text_Editor.py
+++ Text_Editor.py
@@ -4,3 +4,8 @@ class Node:
       self.data = data
       self.next = None
       self.prev = None
+      
+# Create the doubly linked list
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None     
